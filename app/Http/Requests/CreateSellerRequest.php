ba965a6d9b858cae17961f3f2c2f34070a32c231
Ing. Gustavo Reyes <gustavo@tecnoweb.mx>
2015-11-22 23:18:05 +0000

    Request de crear y editar los vendedores

---
+++ app/Http/Requests/CreateSellerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ARM\Http\Requests;
+
+use ARM\Http\Requests\Request;
+
+class CreateSellerRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'email' => 'required'
+        ];
+    }
+}
