4fd90b0219bf909c6362a3d36fa91c55a9163982
Ing. gustavo Reyes <gustavo@tecnoweb.mx>
2015-12-08 23:51:40 +0000

    Agregue el array para mostrar el select la disponibilidad del equipo

---
+++ config/options.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'lista' => [
+        '' => 'Seleccione',
+        '1' => 'Si',
+        '2' => 'No'
+    ],
+
+    'motor' => [
+        '1' => 'Electrico',
+        '2' => 'Diesel',
+        '3' => 'Gas L.P.'
+    ],
+
+    'disponibilidad' => [
+        '1' => 'Disponible',
+        '2' => 'Rentada',
+        '3' => 'En Mantenimiento',
+        '4' => 'Vendida'
+    ]
+];
+?>
