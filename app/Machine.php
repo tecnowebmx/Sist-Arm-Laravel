881a4398bbfb19f0dffa9eb261b3421da130d5e0
Ing. gustavo Reyes <gustavo@tecnoweb.mx>
2015-12-08 23:52:19 +0000

    Agregue a la función fillable el campo availability para al editar el estatus del equipo se guarde en la bd

--- app/Machine.php
+++ app/Machine.php
@@ -8,6 +8,6 @@ class Machine extends Model
 {
     protected $fillable = [
         'name', 'daily_price', 'weekly_price', 'monthly_price', 'brand', 'model', 'series', 'year', 'original_number',
-        'horometro', 'motor', 'costo_adquisicion', 'sale_price', 'acquisition_cost'
+        'horometro', 'motor', 'costo_adquisicion', 'sale_price', 'acquisition_cost', 'availability'
     ];
 }
